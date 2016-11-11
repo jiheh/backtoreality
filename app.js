7bca7154d2a64377b0b17249978b32376fdad851
Jiheh Ritterling <jritterling@gmail.com>
2016-11-11 15:34:06 -0500

    test zoom

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = new Express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use('/', Express.static('views'));
+app.use('/', Express.static('browser'));
 
 app.listen(app.get('port'), function() {
 	console.log('Listening to port 3000');
