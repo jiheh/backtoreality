43d4c6c2e00edbe0014a495be026ba263a60aadb
Jiheh Ritterling <jritterling@gmail.com>
2016-11-11 14:30:10 -0500

    heroku work!

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const Express = require('express');
 
 const app = new Express();
 
+app.set('port', (process.env.PORT || 3000));
+
 app.use('/', Express.static('views'));
 
-app.listen(3000, function() {
-	console.log('Listening to port 3000');
-});
+// app.listen(3000, function() {
+// 	console.log('Listening to port 3000');
+// });
