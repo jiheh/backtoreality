e0ab0667fea69fa10f3a43a1c99d773827738d5f
Jiheh Ritterling <jritterling@gmail.com>
2016-11-14 10:25:55 -0500

    black background video

--- browser/js/test.js
+++ browser/js/test.js
@@ -29,7 +29,7 @@ function init() {
 
   renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
   renderer.autoClear = false;
-  renderer.setClearColor(0xffffff);
+  renderer.setClearColor(0x000000);
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -150,7 +150,7 @@ var planeMesh= new THREE.Mesh( geometry, material );
 scene.add(planeMesh);
 
 // create the dom Element
-  var url = 'https://www.youtube.com/embed/7Uyiew5VsiE';
+  var url = 'https://www.youtube.com/embed/dQw4w9WgXcQ';
   var domElement  = document.createElement('iframe');
   domElement.src  = url;
   domElement.style.border = 'none';
@@ -258,19 +258,19 @@ cssScene.add(cssObject);
   group1.position.x = -400;
   group1.position.z = 300;
   group1.rotation.x = -1.87;
-  scene.add( group1 );
+  // scene.add( group1 );
 
   group2 = THREE.SceneUtils.createMultiMaterialObject( geometry2, materials );
   group2.position.x = 400;
   group2.position.z = 300;
   group2.rotation.x = 0;
-  scene.add( group2 );
+  // scene.add( group2 );
 
   group3 = THREE.SceneUtils.createMultiMaterialObject( geometry3, materials );
   group3.position.x = 0;
   group3.position.z = 300;
   group3.rotation.x = 0;
-  scene.add( group3 );
+  // scene.add( group3 );
 
 
   window.addEventListener( 'mousemove', onDocumentMouseMove, false );
